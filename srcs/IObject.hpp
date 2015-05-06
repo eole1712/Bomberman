40671213ac36f96b5a3216ca64d7c4ff0a772ffe
ghukas_g <ghukas_g@epitech.eu>
2015-05-06 20:46:37 +0200

    add of IObject and Player.hpp

---
+++ srcs/IObject.hpp
@@ -0,0 +1,30 @@
+//
+// IMapObject.hpp for  in /home/ghukas_g/rendu/cpp_bomberman/srcs
+//
+// Made by Grisha GHUKASYAN
+// Login   <ghukas_g@epitech.net>
+//
+// Started on  Wed May  6 19:39:18 2015 Grisha GHUKASYAN
+// Last update Wed May  6 19:59:20 2015 Grisha GHUKASYAN
+//
+
+#ifndef IOBJECT_H_
+# define IOBJECT_H_
+
+namespace Map
+{
+
+enum	ObjectType {Bomb, Player, Bonus, Wall, DestroyableWall};
+
+class	IObject
+{
+public:
+  virtual		~IObject() {}
+
+public:
+  virtual ObjectType	getType() const = 0;
+};
+
+}
+
+#endif /* !IOBJECT_H_ */
---
+++ srcs/Player.hpp
@@ -0,0 +1,76 @@
+//
+// Player.hpp for  in /home/ghukas_g/rendu/cpp_bomberman/srcs
+//
+// Made by Grisha GHUKASYAN
+// Login   <ghukas_g@epitech.net>
+//
+// Started on  Wed May  6 19:57:54 2015 Grisha GHUKASYAN
+// Last update Wed May  6 20:42:44 2015 Grisha GHUKASYAN
+//
+
+#ifndef PLAYER_H_
+# define PLAYER_H_
+
+# include <string>
+# include <list>
+# include "IObject.hpp"
+
+namespace Bomberman
+{
+
+class Player
+  : public Map::IObject
+{
+public:
+  Player();
+  virtual ~Player();
+
+private:
+  Player(const Player &);
+  Player&		operator=(const Player &);
+
+public:
+  std::string const&	getName() const;
+  unsigned int		getRange() const;
+  unsigned int		getSpeed() const;
+
+private:
+  std::string		_name;
+
+public:
+  void			incRange();
+  void			decRange();
+  void			resetRange();
+
+private:
+  unsigned int		_range;
+  static const
+  unsigned int		defRange;
+
+public:
+  void			incSpeed();
+  void			decSpeed();
+  void			resetSpeed();
+
+private:
+  unsigned int		_speed;
+  static const unsigned int defSpeed;
+
+public:
+  bool			isAlive() const;
+
+private:
+  bool			_isAlive;
+
+public:
+  void			addBuff(IBuff *);
+  void			delBuff(IBuff *);
+  void			checkBuffList();
+
+private:
+  std::list<IBuff*>	_buff;
+};
+
+}
+
+#endif /* !PLAYER_H_ */
