58f84d2a3add350f3bbed5064f9b350b221e1bfc
malbra_t <malbra_t@epitech.eu>
2015-05-12 17:25:22 +0200

    Libinit

--- LibBomberman_linux_x64/includes/Game.hh
+++ LibBomberman_linux_x64/includes/Game.hh
@@ -2,15 +2,15 @@
 
 namespace	gdl
 {
-	class Game
-	{
-	public:
-		Game();
-		virtual ~Game();
+class Game
+{
+public:
+  Game();
+  virtual ~Game();
 
-		// Pure virtual function used for Initialize, update the game logic and draw the 3D scene
-		virtual bool initialize() = 0;
-		virtual bool update() = 0;
-		virtual void draw() = 0;
-	};
+  // Pure virtual function used for Initialize, update the game logic and draw the 3D scene
+  virtual bool initialize() = 0;
+  virtual bool update() = 0;
+  virtual void draw() = 0;
+};
 }
