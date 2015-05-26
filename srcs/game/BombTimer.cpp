62f2b0b61e28477bb6eb9929a128a3f7502dc29c
ghukas_g <ghukas_g@epitech.eu>
2015-05-26 15:48:39 +0200

    Ajout de IBomb BombTimer et de la repo bombs avec un classic qui explose apres 3 secondes

---
+++ srcs/game/BombTimer.cpp
@@ -0,0 +1,42 @@
+#include "BombTimer.hpp"
+
+namespace Bomberman
+{
+
+BombTimer::BombTimer(IObject *player, unsigned int range, IBomb *bomb)
+  : Timer(bomb->getDuration()), _player(player), _range(range), _bomb(bomb)
+{
+  start();
+}
+
+BombTimer::~BombTimer()
+{
+
+}
+
+unsigned int	BombTimer::getRange() const
+{
+  return _range;
+}
+
+IObject*	BombTimer::getPlayer() const
+{
+  return _player;
+}
+
+IBomb*		BombTimer::getBombType() const
+{
+  return _bomb;
+}
+
+IObject::Type	BombTimer::getObjectType() const
+{
+  return IObject::BOMB;
+}
+
+bool		BombTimer::isNull() const
+{
+  return false;
+}
+
+}
---
+++ srcs/game/BombTimer.hpp
@@ -0,0 +1,40 @@
+#ifndef BOMBTIMER_H_
+# define BOMBTIMER_H_
+
+# include "Timer.hpp"
+# include "IObject.hpp"
+# include "Player.hpp"
+# include "IBomb.hpp"
+
+namespace Bomberman
+{
+
+class BombTimer
+  : public IObject, public Timer
+{
+public:
+  BombTimer(IObject *, unsigned int, IBomb *);
+  virtual ~BombTimer();
+
+private:
+  BombTimer(const BombTimer &);
+  BombTimer &operator=(const BombTimer &);
+
+public:
+  unsigned int	getRange() const;
+  IObject*	getPlayer() const;
+  IBomb*	getBombType() const;
+
+private:
+  IObject*	_player;
+  unsigned int	_range;
+  IBomb*	_bomb;
+
+public:
+  IObject::Type	getObjectType() const;
+  bool		isNull() const;
+};
+
+}
+
+#endif /* !BOMBTIMER_H_ */
