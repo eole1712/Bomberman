60380ad53c030fc939ea608042e19bc9c0b1a78e
harnay_a <harnay_a@epitech.eu>
2015-06-08 18:25:54 +0200

    début de maplist

---
+++ srcs/core/MapList.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include "JSONDoc.hpp"
+#include "MapList.hpp"
+
+namespace Bomberman
+{
+
+MapList::MapList()
+{}
+
+MapList::~MapList()
+{}
+
+Map*	MapList::getMap(const std::string &name) const
+{
+  if (_json.parse(name + ".json"))
+    return (_json.unserialize<Bomberman::Map>());
+  return (NULL);
+}
+
+void	MapList::addMap(Map* map)
+{
+  this->_maps.insert(map->getName(), std::make_pair(map->getWidth(), map->getHeight()));
+}
+
+std::unordered_map<std::string, std::pair<unsigned int, unsigned int> >	MapList::getMapList() const
+{
+  return (this->_maps);
+}
+
+bool	MapList::operator==(MapList const& other) const
+{
+  return _maps == other._maps;
+}
+
+}
