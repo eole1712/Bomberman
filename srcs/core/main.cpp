3b34cce5cf3cf4bdac17c9a2033abade7f26f971
gamain_j <gamain_j@epitech.eu>
2015-06-11 18:04:29 +0200

    affichage fps

--- srcs/core/main.cpp
+++ srcs/core/main.cpp
@@ -1,5 +1,27 @@
 #include "Core.hpp"
 
+void			showfps()
+{
+  static unsigned int	lasttime = 0;
+  static unsigned int	nbframe = 0;
+  unsigned int		curtime;
+  unsigned int		fps;
+  static unsigned int	maxfps = 0;
+
+  curtime = (unsigned)time(NULL);
+  if ((fps = curtime - lasttime) > 0)
+    {
+      fps = nbframe / fps;
+      if (maxfps < fps)
+	maxfps = fps;
+      printf("%d fps - %d max\n", fps, maxfps);
+      lasttime = curtime;
+      nbframe = 0;
+    }
+  else
+    nbframe++;
+}
+
 int main()
 {
   Bomberman::Core	engine;
@@ -12,6 +34,7 @@ int main()
   engine.startGame();
   while (engine.update() == true)
     {
+      showfps();
       engine.draw();
     }
   return EXIT_SUCCESS;
